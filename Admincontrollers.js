b92ee70ed6970baf2a84892ef1206f13b3d367d0
Omar-Riad04 <omerriad69@yahoo.com>
2024-06-19 19:01:31 +0300

    Update Admincontrollers.js

--- Admincontrollers.js
+++ Admincontrollers.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-
 const editflight=require('../Models/Flights').editflight; 
-
-
 
 exports.updateflight = async (req, res) => {
   const { flightnumber, Currentorigin, Currentdestination, Currentdtime, Currentatime, neworigin, newdestination, newdtime, newatime } = req.body;
-
   try {
       const flight = await addflight.findOne({ 
           addflightnumber: flightnumber,
@@ -16,12 +12,9 @@ exports.updateflight = async (req, res) => {
           addflightdtime: Currentdtime,
           addflightatime: Currentatime
       });
-
       if (!flight) {
           return res.status(404).send('Flight not found');
       }
-
-      // Update flight's information
       flight.addflightorigin = neworigin;
       flight.addflightnumberdestination = newdestination;
       flight.addflightdtime = newdtime;
