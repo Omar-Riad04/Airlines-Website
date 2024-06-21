ea12cfe69f7218f75983e3d2307f3e75f887283d
Mohsen-Samer <mohsen2207017@miuegypt.edu.eg>
2024-06-21 17:43:17 +0300

    Add files via upload

--- User.js
+++ User.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Usercontrollers = require('../Controllers/Usercontrollers');
-router.post('/submitbook', Usercontrollers.submitbookingForm);
+
+
+router.post('/searchflights', Usercontrollers.searchFlights);
+
+
+
+
+
 router.post('/submitpay', Usercontrollers.submitpayform);
+
+
+
+
 module.exports = router;
