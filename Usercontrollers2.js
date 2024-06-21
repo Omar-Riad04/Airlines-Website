4363b2e95093070bbb84d4c5d9a70f42505bec18
FadyWael10 <169147111+FadyWael10@users.noreply.github.com>
2024-06-21 18:21:38 +0300

    Add files via upload

--- Usercontrollers2.js
+++ Usercontrollers2.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const Mysignadd=require('../Models/Users').Mysign;
+const MysignaddIN=require('../Models/Users').MysignIN;
 exports.submitadduserform = async (req, res) => {
     const {UserNamee,Passwordd,Emaill} = req.body;
 
@@ -17,3 +18,29 @@ exports.submitadduserform = async (req, res) => {
 };
 
 
+exports.signInForm = async (req, res) => {
+    const { UserNameeIN,
+    PassworddIN} = req.body;
+
+    try {
+        const mysignin = new MysignaddIN({ UserNameeIN,
+            PassworddIN });
+
+        const search = {
+            UserNamee: mysignin.UserNameeIN,
+            Passwordd: mysignin.PassworddIN,
+
+        };
+
+        const flightsin = await Mysignadd.find(search);
+
+        if (flightsin.length === 0) {
+            res.send('No User found.');
+        } else {
+            res.render('index2', { flightsin });
+        }
+    } catch (err) {
+        console.error('Error searching User:', err);
+        res.status(500).send('Failed to search User');
+    }
+};
