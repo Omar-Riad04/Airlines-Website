d11c9c7c9fdaf0325c24fbaec7a4b4c34b984ad0
Omar-Riad04 <omerriad69@yahoo.com>
2024-06-21 17:48:20 +0300

    Add files via upload

--- User1.js
+++ User1.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Usercontrollers = require('../Controllers/Usercontrollers');
+
 router.post('/submit', Usercontrollers.submitcontactForm);
+
+
+router.post('/submitstatus', Usercontrollers.searchstatus);
+
+
 module.exports = router;
