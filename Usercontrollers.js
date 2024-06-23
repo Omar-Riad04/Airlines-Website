5dc5b13a2057f155d007ebb9f8a1117a6473b96d
Mohsen-Samer <mohsen2207017@miuegypt.edu.eg>
2024-06-23 03:44:55 +0300

    Update Usercontrollers.js

--- Usercontrollers.js
+++ Usercontrollers.js
@@ -6,23 +6,33 @@ const express = require('express');
    
     
 
+
 exports.searchFlights = async (req, res) => {
     const { from, to, departureDate, departureTime } = req.body;
 
     try {
-        const mybook = new Mybooking({ from, to, departureDate, departureTime });
+        const departureDateTime = new Date(`${departureDate}T${departureTime}`);
 
-        const search = {
-            addflightorigin: mybook.from,
-            addflightnumberdestination: mybook.to,
-            addflightddate: mybook.departureDate,
-            addflightdtime: mybook.departureTime
-        };
+        const flights = await addflight.find({
+            addflightorigin: from,
+            addflightnumberdestination: to,
+            $or: [
+            
+                {
+                    $and: [
+                        { addflightddate: { $eq: departureDateTime.toISOString().split('T')[0] } },
+                        { addflightdtime: { $gte: departureDateTime.toISOString().split('T')[1] } }
+                    ]
+                },
 
-        const flights = await addflight.find(search);
+                {
+                    addflightddate: { $gt: departureDateTime.toISOString().split('T')[0] }
+                }
+            ]
+        });
 
         if (flights.length === 0) {
-            res.send('No flights found.');
+            res.render('index2')
         } else {
             res.render('bookTickets', { flights });
         }
@@ -33,19 +43,41 @@ exports.searchFlights = async (req, res) => {
 };
 
 
+
 exports.submitpayform = async (req, res) => {
     const {name,ElementInternals,address,city,state,zipCode,nameOnCard,cardNumber,expMonth,expYear,cvv  } = req.body;
 
     try {
-        const newUserpay = new Mypay({  name,ElementInternals,address,city,state,zipCode,nameOnCard,cardNumber,expMonth,expYear,cvv });
+        let newUserpay = await Mypay.findOne({
+          
+            expMonth: expMonth
+        });
+
+        if (!newUserpay) {
+            return res.status(404).send('User payment data not found');
+        }
+
+newUserpay.name=name
+newUserpay.ElementInternals=ElementInternals
+newUserpay.address=address
+newUserpay.city=city
+newUserpay.state=state
+newUserpay.zipCode=zipCode
+newUserpay.nameOnCard=nameOnCard
+newUserpay.cardNumber=cardNumber
+newUserpay.expMonth=expMonth
+newUserpay.expYear=expYear
+newUserpay.cvv=cvv
+
         await newUserpay.save();
 
-        res.render('pay');
+        res.render('index2');
     } catch (err) {
         console.error('Error saving user:', err);
         res.status(500).send('Failed to submit form');
     }
 };
+
 
 
 
